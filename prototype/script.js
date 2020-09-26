07462912f862bac70126bc5091e45233bf0af733
jp7webdesign <jpaulo6ba@gmail.com>
2020-09-26 14:20:27 -0300

    finished prototype

--- prototype/script.js
+++ prototype/script.js
@@ -45,10 +45,50 @@
 // console.log(listaArray1);
 // console.log(listaArray2);
 
-const Carro = {
-  marca: "Ford",
-  preco: 2000,
-  andar() {
-    return true;
-  },
+// const Carro = {
+//   marca: "Ford",
+//   preco: 2000,
+//   andar() {
+//     return "oi";
+//   },
+// };
+
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoa.prototype.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
 };
+
+const andre = new Pessoa("André", "Rafael", 28);
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+// li.click.constructor.name
+li; //HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // Undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // String
