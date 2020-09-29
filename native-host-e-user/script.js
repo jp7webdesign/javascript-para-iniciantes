bf3bb79466c58457be05fbfd860959f573412a61
jp7webdesign <jpaulo6ba@gmail.com>
2020-09-29 09:23:15 -0300

    add string

--- native-host-e-user/script.js
+++ native-host-e-user/script.js
@@ -1,29 +1,40 @@
-// console.log(typeof Array.fromlklklk !== "undefined");
+// const comida = "Pizza";
+// const frase = "A melhor comida";
 
-// if (typeof Array.fromlklklk !== "undefined") {
-//   console.log("Existe");
-// } else {
-//   console.log("Não existe");
-// }
+// console.log(comida.length);
 
-// Liste 5 objetos nativos
-// Object
-// Array
-// String
-// Number
-// Function
+// console.log(frase[frase.length - 1]);
+// console.log(frase.charAt(0));
+// -----
 
-// Liste 5 objetos do browser
-// Window
-// History
-// Document
-// HTMLCollection
-// NodeList
+// const frase = "A melhor linguagem é ";
+// const linguagem = "JavaScript";
 
-// Liste 2 Métodos, Propriedades ou Objetos
-// presentes no Chrome que não existem no Firefox
-if (typeof document.webkitHidden !== "undefined") {
-  console.log("Existe");
-} else {
-  console.log("Não existe");
-}
+// const fraseFinal = frase.concat(linguagem, "!");
+// console.log(fraseFinal);
+// -----
+
+// const fruta = "Banana";
+// const listaFrutas = "Melancia, Banana, Laranja";
+
+// console.log(listaFrutas.includes(fruta, 14));
+// console.log(fruta.includes(listaFrutas));
+
+// console.log(fruta.endsWith("na"));
+
+// const transacao1 = "Depósito de cliente";
+// const transacao2 = "Depósito de fornecedor";
+// const transacao3 = "Taxa de camisas";
+
+// console.log(transacao1.slice(0, 4));
+
+// console.log(fruta.indexOf("n"));
+// console.log(fruta.lastIndexOf("Ba"));
+// -----
+
+const listaPrecos = ["R$ 99", "R$ 199", "R$ 1200"];
+
+listaPrecos.forEach((preco) => {
+  console.log(preco.padStart(10, "."));
+  console.log(preco.padEnd(10, "."));
+});
