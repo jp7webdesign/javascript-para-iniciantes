36e47a5cc0c3e5154db467f0f54be8ab28045f88
jp7webdesign <jpaulo6ba@gmail.com>
2020-09-29 08:51:33 -0300

    finish native-host-e-user

--- native-host-e-user/script.js
+++ native-host-e-user/script.js
@@ -1,6 +1,28 @@
-console.log(typeof Array.fromlklklk !== "undefined");
+// console.log(typeof Array.fromlklklk !== "undefined");
 
-if (typeof Array.fromlklklk !== "undefined") {
+// if (typeof Array.fromlklklk !== "undefined") {
+//   console.log("Existe");
+// } else {
+//   console.log("Não existe");
+// }
+
+// Liste 5 objetos nativos
+// Object
+// Array
+// String
+// Number
+// Function
+
+// Liste 5 objetos do browser
+// Window
+// History
+// Document
+// HTMLCollection
+// NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+if (typeof document.webkitHidden !== "undefined") {
   console.log("Existe");
 } else {
   console.log("Não existe");
