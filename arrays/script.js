1e4fd609db082084004ee0db90475b7db17a5f38
jp7webdesign <jpaulo6ba@gmail.com>
2021-04-09 13:30:13 -0300

    finished arrays2

--- arrays/script.js
+++ arrays/script.js
@@ -50,13 +50,123 @@
 
 
 
-const carros = ['Ford', 'Fiat', 'VW'];
-carros.unshift('Honda', 'Kia'); // 5
-console.log(carros); // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
+// const carros = ['Ford', 'Fiat', 'VW'];
+// carros.unshift('Honda', 'Kia'); // 5
+// console.log(carros); // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW'];
 
-carros.push('Ferrari'); // 6
-console.log(carros); // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
+// carros.push('Ferrari'); // 6
+// console.log(carros); // ['Honda', 'Kia', 'Ford', 'Fiat', 'VW', 'Ferrari'];
 
-console.log(carros.reverse());
-console.log(carros.pop());
+// console.log(carros.reverse());
+// console.log(carros.pop());
+// console.log(carros);
+
+// const carros = ['Ford', 'Fiat', 'VW', 'Honda' ];
+// carros.splice(1, 0, 'Kia', 'Mustang');
+// console.log(carros);
+
+// carros.splice(3, 2, 'Ferrari');
+// console.log(carros);
+
+// console.log(['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3));
+// // ['Item1', 'Item2', 'Item1', 'Item2']
+
+// console.log(['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1));
+// // ['Item1', 'Item2', 'Item3', 'Item1']
+
+// console.log(['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana'));
+// // ['Banana', 'Banana', 'Banana', 'Banana']
+
+// console.log(['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 2));
+// // ['Item1', 'Item2', 'Banana', 'Banana']
+
+// console.log(['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 1, 3));
+// // ['Item1', 'Banana', 'Banana', 'Item4']
+
+// const transporte1 = ['Barco', 'Aviao'];
+// const transporte2 = ['Carro', 'Moto'];
+// const transportes = transporte1.concat(transporte2);
+// // ['Barco', 'Aviao', 'Carro', 'Moto'];
+// console.log(transportes);
+
+// const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// // ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+// console.log(maisTransportes);
+
+// const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+// console.log(linguagens.includes('css')); // true
+// console.log(linguagens.includes('ruby')); // false
+// console.log(linguagens.indexOf('python')); // 4
+// console.log(linguagens.indexOf('js')); // 2
+// console.log(linguagens.lastIndexOf('js')); // 5
+
+// const linguagens = ['html', 'css', 'js', 'php', 'python'];
+// console.log(linguagens.join()); // 'html,css,js,php,python'
+// console.log(linguagens.join(' ')); // 'html css js php python'
+// console.log(linguagens.join(' -_- ')); // 'html-_-css-_-js-_-php-_-python'
+
+// let htmlString = '<h2>Título Principal</h2>'
+// console.log(htmlString = htmlString.split('h2'));
+// // ['<', '>Título Principal</', '>']
+// console.log(htmlString = htmlString.join('h1'));
+// // <h1>Título Principal</h1>
+
+// const linguagens = ['html', 'css', 'js', 'php', 'python'];
+// console.log(linguagens.slice(3)); // ['php', 'python']
+// console.log(linguagens.slice(1, 4)); // ['css', 'js', 'php']
+
+// const cloneLinguagens = linguagens.slice();
+
+//Exercícios
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const primeiroValor = comidas[0];
+console.log(comidas);
+console.log(primeiroValor);
+comidas.shift();
+console.log(comidas);
+// Remova o último valor de comidas e coloque em uma variável
+const ultimoValor = comidas[comidas.length -1];
+console.log(ultimoValor);
+comidas.pop();
+console.log(comidas);
+// Adicione 'Arroz' ao final da array
+comidas.push('Arroz');
+console.log(comidas);
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift('Peixe', 'Batata');
+console.log(comidas);
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+console.log(estudantes.sort());
+// Inverta a ordem dos estudantes
+console.log(estudantes.reverse());
+// Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'));
+// Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'));
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+console.log(html = html.split('section'));
+console.log(html = html.join('ul'));
+console.log(html = html.split('div'));
+console.log(html = html.join('li'));
+
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const backupCarros = Array.from(carros);
+console.log(backupCarros);
+carros.pop();
 console.log(carros);
+
+
