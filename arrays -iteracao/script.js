f5d68de340d6b7683ee7e540d87419963ff30f62
jp7webdesign <jpaulo6ba@gmail.com>
2021-04-13 17:46:35 -0300

    finished array and interations 4

--- arrays -iteracao/script.js
+++ arrays -iteracao/script.js
@@ -105,27 +105,95 @@
 // const buscaMaior45 = numeros.filter(x => x > 45);
 // console.log(buscaMaior45);
 
-const aulas = [
+// const aulas = [
+//   {
+//     nome: 'HTML 1',
+//     min: 15
+//   },
+//   {
+//     nome: 'HTML 2',
+//     min: 10
+//   },
+//   {
+//     nome: 'CSS 1',
+//     min: 20
+//   },
+//   {
+//     nome: 'JS 1',
+//     min: 25
+//   },
+// ]
+
+// const aulasMaiores = aulas.filter((aula) => { 
+//   return aula.min > 15;
+// });
+// // [{nome: 'CSS 1', min: 20}, {nome: 'JS 1', min: 25}]
+// console.log(aulasMaiores);
+
+//exercício
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
+const arrayCursos = Array.from(cursos);
+
+const objetosCurso = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector('h1').innerText;
+  const descricao = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas
+  }
+})  
+
+console.log(objetosCurso);
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQue100 = numeros.filter(n => n > 100);
+console.log(maioresQue100);
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const possuiBaixo = instrumentos.some((item) => {
+  return item === 'Baixo';
+})
+
+console.log(possuiBaixo);
+
+// Retorne o valor total das compras
+const compras = [
   {
-    nome: 'HTML 1',
-    min: 15
+    item: 'Banana',
+    preco: 'R$ 4,99'
   },
   {
-    nome: 'HTML 2',
-    min: 10
+    item: 'Ovo',
+    preco: 'R$ 2,99'
   },
   {
-    nome: 'CSS 1',
-    min: 20
+    item: 'Carne',
+    preco: 'R$ 25,49'
   },
   {
-    nome: 'JS 1',
-    min: 25
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
   },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
 ]
 
-const aulasMaiores = aulas.filter((aula) => { 
-  return aula.min > 15;
-});
-// [{nome: 'CSS 1', min: 20}, {nome: 'JS 1', min: 25}]
-console.log(aulasMaiores);
+const valorTotal = compras.reduce((acumulador, item) => {
+  const precoLimpo = +item.preco.replace('R$', '').replace(',', '.');
+  return acumulador + precoLimpo;
+}, 0);
+
+console.log(valorTotal);
